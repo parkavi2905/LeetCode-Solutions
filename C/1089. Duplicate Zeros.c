8ac5251af53169edcd944361daf2f900295c00ce
Parkavi <133233046+parkavi2905@users.noreply.github.com>
2024-05-06 13:46:35 +0530

    Create 1089. Duplicate Zeros.c

---
+++ C/1089. Duplicate Zeros.c
@@ -0,0 +1,11 @@
+void duplicateZeros(int* arr, int arrSize) {
+    int i, j;
+    for (i = 0; i < arrSize; i++) {
+        if (arr[i] == 0) {          
+            for (j = arrSize - 1; j > i; j--) {
+                arr[j] = arr[j - 1];
+            }            
+            i++;
+        }
+    }
+}
